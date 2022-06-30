34b1e5e00efa1c86db1b3243aff50445f1703fa5
Pokesi <81540683+Pokesi@users.noreply.github.com>
2022-06-30 20:09:14 +1000

    Update NetworkConnector.ts

--- src/entities/connectors/NetworkConnector.ts
+++ src/entities/connectors/NetworkConnector.ts
@@ -90,7 +90,7 @@ class MiniRpcProvider implements AsyncSendable {
       memo[current.request.id] = current
       return memo
     }, {})
-    for (const result of json.data) {
+    for (const result of Object.keys(json)) {
       const {
         resolve,
         reject,
